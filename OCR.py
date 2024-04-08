aef93d419b0e9f5c13af4813107bc2d2d39c9035
JeongGG8 <113495369+JeongGG8@users.noreply.github.com>
2024-04-08 14:55:31 +0900

    Update OCR.py

--- OCR.py
+++ OCR.py
@@ -11,7 +11,6 @@ image = cv2.imread(path)
 
 
 gray = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
-##blurred = cv2.GaussianBlur(gray, (5,5), 0)
 edged = cv2.Canny(gray, 75, 200)
 
 ##cnts = cv2.findContours(edged.copy(), cv2.RETR_EXTERNAL,cv2.CHAIN_APPROX_SIMPLE)
@@ -20,12 +19,20 @@ edged = cv2.Canny(gray, 75, 200)
 
 ##receiiptCnt = None
 
-##for c in cnts:
-##    peri = cv2.arcLength(c, True)
-##    approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+for c in cnts:
+  peri = cv2.arcLength(c, True)
+  approx = cv2.approxPolyDP(c, 0.02 * peri, True)
+  if len(approx) == 4:
+      receiiptCnt = approx
+      break
+if receiiptCnt is None:
+    raise Exception(("Could not find receipt outline"))
+
+result = image.copy()
+cv2.drawContours(result, [receiiptCnt], -1, (0, 255, 0), 2)
 
 text = pytesseract.image_to_string(edged, lang='eng')
-
 print(text)
+
 cv2.imshow("result", edged)
 cv2.waitKey()
